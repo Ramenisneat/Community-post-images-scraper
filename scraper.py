933cb7d0b927c31c489766aeaaee3319732bee97
Ramenisneat <rahulmyana42@gmail.com>
2023-01-25 23:33:38 -0600

    SILVERVALE UPDATE

--- scraper.py
+++ scraper.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 import os
-from decouple import config
+from dotenv import load_dotenv
 from datetime import datetime
 import subprocess
 import argparse
 
-def get_channel():
+
+
+def get_channel(CHANNEL):
     op = webdriver.ChromeOptions()
     op.add_argument("headless")
-    driver = webdriver.Chrome(executable_path="/usr/lib/chromium/chromedriver",options=op) 
-    driver.get(url= f"https://www.youtube.com/c/{config('CHANNEL')}/community")
+    driver = webdriver.Chrome(options=op) 
+    driver.get(url= f"https://www.youtube.com/@{CHANNEL}/community")
     sleep(1)
-    print(f"Got {config('CHANNEL')} channel")
+    print(f"Got {CHANNEL} channel")
     return driver
 
 def scroll_to_bottom(driver):
@@ -26,48 +29,39 @@ def scroll_to_bottom(driver):
         offset = driver.execute_script("return window.pageYOffset")
         if offset == current:
             break
+
     sleep(1)
     print("scrolled to the bottom")
     
 
 def get_posts(driver):
-    container=driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-backstage-items/ytd-item-section-renderer/div[3]")
-    posts = container.find_elements_by_class_name("style-scope ytd-backstage-post-renderer")
-
+    posts = driver.find_elements(By.XPATH, "//div[@id='contents']//div[@id='content-attachment']//a[1]")
     post_links=[]
-
     for e in posts:
-        try:
-            e = e.find_element_by_id("content-attachment")
-            e = e.find_element_by_tag_name('ytd-backstage-image-renderer')
-            e = e.find_element_by_tag_name('a')
-
-            post_links.append(e.get_attribute("href"))
-        except:
-            pass
+        link = e.get_attribute("href")
+        if "post/" in link:
+            post_links.append(link)
 
     print("Got the links of posts")
+    # print(post_links)
     return post_links
 
 
-def scraper(driver,post_links):
+def scraper(driver,post_links,FOLDER):
 
-    if os.path.exists(f"{config('FOLDER')}"):
-        os.system(f"rm -rf {config('FOLDER')}")
+    if os.path.exists(f"{FOLDER}"):
+        os.system(f"rm -rf {FOLDER}")
 
-    os.mkdir(config('FOLDER'))
-    os.chdir(config('FOLDER'))
+    os.mkdir(FOLDER)
+    os.chdir(FOLDER)
     
     counter = 1
     with open("urls.txt",mode="w") as file:
         for url in post_links[::-1]:
-            if url == None:
-                print("none")
-                post_links.remove(url)
-                continue
-            print(url)
+            print("making " + url)
             driver.get(url=url)
-            i = driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-backstage-items/ytd-item-section-renderer/div[3]/ytd-backstage-post-thread-renderer/div[1]/ytd-backstage-post-renderer/div[1]/div[2]/div[2]/ytd-backstage-image-renderer/div/yt-img-shadow/img")
+            sleep(1)
+            i = driver.find_element(By.XPATH, "//div[@id='image-container']//img")
             link = i.get_attribute("src")
             os.system(f"curl {link} > image{counter}.png")
             sleep(1)
@@ -75,54 +69,58 @@ def scraper(driver,post_links):
             file.write(url+"\n")
 
 
-def updater(driver, post_links):
+def updater(driver, post_links, FOLDER):
 
     print(datetime.now())
     sleep(2)
 
-    os.chdir(config('FOLDER'))
+    os.chdir(FOLDER)
   
-    num = int(subprocess.check_output("ls | wc -l", shell=True)) - 1
+    num = int(subprocess.check_output("cat urls.txt | wc -l", shell=True)) 
     lines = []
-    with open('urls.txt') as f:
-        lines = f.read().splitlines()
-    print(num)
+    with open("urls.txt",mode="r") as file:
+        lines = file.read().splitlines()
+        print(num)
+    
     with open("urls.txt",mode="a") as file:
         for url in post_links[::-1]:
-            if url == None: continue
             if url in lines:
                 print(f"already got {url}")
             else:
                 driver.get(url=url)
-                src = (driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-backstage-items/ytd-item-section-renderer/div[3]/ytd-backstage-post-thread-renderer/div[1]/ytd-backstage-post-renderer/div[1]/div[2]/div[2]/ytd-backstage-image-renderer/div/yt-img-shadow/img")).get_attribute("src")
+                sleep(1)
+                src = (driver.find_element(By.XPATH, "//div[@id='image-container']//img")).get_attribute("src")
                 print("making "+url)
-                num = num +1
+                num = num + 1
                 os.system(f"curl {src} > image{num}.png")
-    
-                sleep(2)
+                sleep(1)
                 print("finished this one")
                 file.write(url+"\n")
 
 
 def main():
+    load_dotenv()
+    CHANNEL = os.getenv("CHANNEL")
+    FOLDER = os.getenv("FOLDER")
+
     parser = argparse.ArgumentParser(description= "Youtube community post images scraper")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-s', help='Scrape all of the available posts', action='store_true')
     group.add_argument('-u', help="Update upon existing scraped images. (Use only after executing '-s')", action='store_true')
     args = parser.parse_args()
 
-    driver = get_channel()
+    driver = get_channel(CHANNEL)
 
     if args.s:
         scroll_to_bottom(driver)
         post_links = get_posts(driver)
-        scraper(driver,post_links)
+        scraper(driver,post_links,FOLDER)
 
     elif args.u:
-        driver.execute_script(f"window.scrollTo(0,1000)")
+        driver.execute_script(f"window.scrollTo(0,2000)")
         sleep(1)
         post_links = get_posts(driver)
-        updater(driver,post_links)
+        updater(driver,post_links,FOLDER)
     
     print("done")
         
